d34a4df647cc6fefd526980eac1ce14430768a55
Yash Gokakkar <yashgokakkar@gmail.com>
2023-07-03 00:13:16 +0530

    Navbar updated

--- webcom/src/App.js
+++ webcom/src/App.js
@@ -1,8 +1,23 @@
 import Sample from "./components/Sample.jsx"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
+
 
 function App() {
   return (
-    <Sample/>
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/society" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact" element={<About />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
